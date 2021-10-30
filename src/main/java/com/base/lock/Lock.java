0f0a85e8ef2d7df5cce2e3b233a6cd7fb9faa6f8
shenli <shenli@tsign.cn>
2021-10-30 19:03:43 +0800

    init interface

--- src/main/java/com/base/lock/Lock.java
+++ src/main/java/com/base/lock/Lock.java
@@ -19,19 +19,19 @@ public interface Lock {
      * try to get lock, wait if not get the lock
      * 尝试获取锁，如果获取不到，会一直阻塞
      *
-     * @throws : 连接断开、interrupt等均会抛出CannotAcquireLockException
+     * @throws : 连接断开、interrupt等均会抛出AcquireLockException
      */
     void lock() throws AcquireLockException;
 
     /**
-     * @param waitTime: 尝试获取锁，若在waitTime时间内获取到了，返回true；否则，在waitTime时间后返回false。
-     *                  方法也有可能在waitTime时间内返回，如果中间发生异常。
+     * @param waitTime:
+     *                try to get lock, if get the lock in waitTime, returrn true, else return fasle
      * @param unit:     时间单位
      */
     boolean tryLock(int waitTime, TimeUnit unit);
 
     /**
-     * 释放锁。如果是可重入锁，在重入次数减为0时释放锁
+     *release lock
      */
     void unlock();
     
